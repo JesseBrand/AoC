193af2809d5b40532865c77710eb9e0d3f9279d4
Jesse Brand <jessebrand@gmail.com>
2023-12-01 09:11:45 +0100

    Tidy up.

--- src/nl/jessebrand/aoc/aoc2023/D01.java
+++ src/nl/jessebrand/aoc/aoc2023/D01.java
@@ -3,17 +3,18 @@ package nl.jessebrand.aoc.aoc2023;
 import static nl.jessebrand.aoc.Utils.readFile;
 
 import java.io.IOException;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class D01 {
 	
-	private static final Map<String, Integer> mapping = buildMap();
+	private static final Map<String, Integer> DIGIT_MAPPING = buildMap();
 
 	public static void main(String[] args) throws IOException {
 		final List<String> lines = readFile("2023/d1");
-		System.out.println(lines);
+//		System.out.println(lines);
 		int total = 0;
 		for (String line : lines) {
 			String first = null;
@@ -26,23 +27,24 @@ public class D01 {
 						first = last;
 					}
 				}
-				for (String mapKey : mapping.keySet()) {
+				// 1B
+				for (String mapKey : DIGIT_MAPPING.keySet()) {
 					if (line.substring(i).startsWith(mapKey)) {
-						last = "" + mapping.get(mapKey);
+						last = "" + DIGIT_MAPPING.get(mapKey);
 						if (first == null) {
 							first = last;
 						}
 					}
 				}
 			}
-			System.out.println(String.format("%s : %s - %s", line, first, last));
+//			System.out.println(String.format("%s : %s - %s", line, first, last));
 			total += Integer.parseInt(first + last);
 		}
 		System.out.println(total);
 	}
 
 	private static Map<String, Integer> buildMap() {
-		Map<String, Integer> result = new HashMap<>();
+		final Map<String, Integer> result = new HashMap<>();
 		result.put("one", 1);
 		result.put("two", 2);
 		result.put("three", 3);
@@ -52,6 +54,6 @@ public class D01 {
 		result.put("seven", 7);
 		result.put("eight", 8);
 		result.put("nine", 9);
-		return result;
+		return Collections.unmodifiableMap(result);
 	}
 }

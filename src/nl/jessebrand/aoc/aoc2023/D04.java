0fd9ef659c473898f6d594e709523e7ec8fafd37
Jesse Brand <jessebrand@gmail.com>
2023-12-03 11:44:26 +0100

    Setup day4/5 empty templates.

---
+++ src/nl/jessebrand/aoc/aoc2023/D04.java
@@ -0,0 +1,15 @@
+package nl.jessebrand.aoc.aoc2023;
+
+import static nl.jessebrand.aoc.Utils.readFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D04 {
+	
+	public static void main(String[] args) throws IOException {
+		final List<String> lines = readFile("2023/d04");
+		System.out.println(lines);
+	}
+
+}

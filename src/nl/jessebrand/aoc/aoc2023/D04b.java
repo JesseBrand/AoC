cda6a92539c14f5018331088879944de894b94cd
Jesse Brand <jessebrand@gmail.com>
2023-12-04 06:23:20 +0100

    2023 day 4 solutions.

--- src/nl/jessebrand/aoc/aoc2023/D04b.java
+++ src/nl/jessebrand/aoc/aoc2023/D04b.java
@@ -4,37 +4,42 @@ import static nl.jessebrand.aoc.Utils.readFile;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public class D04 {
+public class D04b {
 	
 	public static void main(String[] args) throws IOException {
 		final List<String> lines = readFile("2023/d04");
 		final List<Card> cards = parseCards(lines);
 		System.out.println(cards);
-		int total = 0;
+		int[] cardCount = new int[cards.size() + 1];
 		for (Card card : cards) {
-			int score = 0;
+			cardCount[card.id()] = 1;
+		}
+		System.out.println(String.format("Start: %s", Arrays.toString(cardCount)));
+		for (Card card : cards) {
 			int matches = 0;
 			for (int yours : card.yourNumbers()) {
 				for (int winning : card.winningNumbers()) {
 					if (yours == winning) {
-						if (score == 0) {
-							score = 1;
-						} else {
-							score *= 2;
-						}
 						matches++;
 						break;
 					}
 				}
 			}
-			total += score;
-			System.out.println(String.format("Card %s has %d matches: %d score", card.id(), matches, score));
+			for (int i = card.id() + 1; i < card.id() + 1 + matches; i++) {
+				cardCount[i] += cardCount[card.id()];
+			}
+			System.out.println(String.format("After card %s (%d matches): %s", card.id(), matches, Arrays.toString(cardCount)));
+		}
+		int total = 0;
+		for (int i : cardCount) {
+			total += i;
 		}
 		System.out.println(total);
 	}
-
+	
 	private record Card(int id, List<Integer> yourNumbers, List<Integer> winningNumbers) {}
 
 	private static List<Card> parseCards(List<String> lines) {

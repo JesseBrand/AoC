f44eaac6360b1fc932e22f4cb5b3e4439cec94dd
Jesse Brand <jessebrand@gmail.com>
2023-12-06 21:44:57 +0100

    Prep next 2 days.

---
+++ src/nl/jessebrand/aoc/aoc2023/D07.java
@@ -0,0 +1,15 @@
+package nl.jessebrand.aoc.aoc2023;
+
+import static nl.jessebrand.aoc.Utils.readFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D07 {
+	
+	public static void main(String[] args) throws IOException {
+		final List<String> lines = readFile("2023/d07");
+		System.out.println(lines);
+	}
+
+}
---
+++ src/nl/jessebrand/aoc/aoc2023/D08.java
@@ -0,0 +1,15 @@
+package nl.jessebrand.aoc.aoc2023;
+
+import static nl.jessebrand.aoc.Utils.readFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D08 {
+	
+	public static void main(String[] args) throws IOException {
+		final List<String> lines = readFile("2023/d08");
+		System.out.println(lines);
+	}
+
+}

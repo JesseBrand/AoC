47d16c3d9594c74de79649e57352b600efffa470
Jesse Brand <jessebrand@gmail.com>
2023-12-09 15:44:12 +0100

    Merged a and b together.

--- src/nl/jessebrand/aoc/aoc2023/D09.java
+++ src/nl/jessebrand/aoc/aoc2023/D09.java
@@ -13,12 +13,32 @@ public class D09 {
 		final List<String> lines = readFile("2023/d09");
 		System.out.println(lines);
 		final List<List<Integer>> ranges = parseRanges(lines);
-		long result = 0;
+		long resultA = 0;
+		long resultB = 0;
 		for (List<Integer> range: ranges) {
-			int next = predictNext(range);
-			result += next;
+			resultA += predictNext(range, new NextValue());
+			resultB += predictNext(range, new PreviousValue());
 		}
-		System.out.println(result);
+		System.out.println("a: " + resultA);
+		System.out.println("b: " + resultB);
+	}
+	
+	private interface NextValueGenerator {
+		int nextValue(List<Integer> range, int nextPrediction);
+	}
+	
+	private static class NextValue implements NextValueGenerator {
+		@Override
+		public int nextValue(final List<Integer> range, final int nextPrediction) {
+			return range.get(range.size() - 1) + nextPrediction;
+		}
+	}
+	
+	private static class PreviousValue implements NextValueGenerator {
+		@Override
+		public int nextValue(final List<Integer> range, final int nextPrediction) {
+			return range.get(0) - nextPrediction;
+		}
 	}
 
 	private static List<List<Integer>> parseRanges(List<String> lines) {
@@ -29,19 +49,19 @@ public class D09 {
 		return result;
 	}
 
-	private static int predictNext(List<Integer> range) {
-		if (allZeroes(range)) {
+	private static int predictNext(List<Integer> range, NextValueGenerator valueGenerator) {
+		if (areAllZeroes(range)) {
 			return 0;
 		}
 		final List<Integer> newRange = new ArrayList<>();
 		for (int i = 1; i < range.size(); i++) {
 			newRange.add(range.get(i) - range.get(i - 1));
 		}
-		int rangeNext = predictNext(newRange);
-		return range.get(0) - rangeNext;
+		int rangeNext = predictNext(newRange, valueGenerator);
+		return valueGenerator.nextValue(range, rangeNext);
 	}
 
-	private static boolean allZeroes(List<Integer> range) {
+	private static boolean areAllZeroes(List<Integer> range) {
 		boolean result = true;
 		for (int i : range) {
 			if (i != 0) {
@@ -50,7 +70,5 @@ public class D09 {
 		}
 		return result;
 	}
-	
-	
 
 }

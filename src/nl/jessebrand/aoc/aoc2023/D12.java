134fc3f9636874ac4de7181ede3a0228ebf80ea2
Jesse Brand <jessebrand@gmail.com>
2023-12-10 10:16:49 +0100

    Prep next days.

---
+++ src/nl/jessebrand/aoc/aoc2023/D12.java
@@ -0,0 +1,15 @@
+package nl.jessebrand.aoc.aoc2023;
+
+import static nl.jessebrand.aoc.Utils.readFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D12 {
+	
+	public static void main(String[] args) throws IOException {
+		final List<String> lines = readFile("2023/d12");
+		System.out.println(lines);
+	}
+
+}

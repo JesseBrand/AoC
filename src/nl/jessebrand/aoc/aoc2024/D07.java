57e73336b955d74603754684a4c72c343473dfa4
Jesse Brand <jessebrand@gmail.com>
2024-12-07 09:09:32 +0100

    Clean and beautify

--- src/nl/jessebrand/aoc/aoc2024/D07.java
+++ src/nl/jessebrand/aoc/aoc2024/D07.java
@@ -13,37 +13,37 @@ public class D07 {
 	private enum Operator {
 		ADD {
 			@Override
-			long apply(long i0, long i1) {
+			long apply(final long i0, final long i1) {
 				return i0 + i1;
 			}
 		},
 		MULTI {
 			@Override
-			long apply(long i0, long i1) {
+			long apply(final long i0, final long i1) {
 				return i0 * i1;
 			}
 		},
 		CONCAT {
 			@Override
-			long apply(long i0, long i1) {
+			long apply(final long i0, final long i1) {
 				return Long.parseLong("" + i0 + i1);
 			}
 		};
 		
-		abstract long apply(long i0, long i1);
+		abstract long apply(final long i0, final long i1);
 	}
 	
 	private record Calibration(long total, List<Long> values) {}
 
 	public static void main(String[] args) throws IOException {
 		final List<String> lines = readFile("2024/d07");
-		final List<Calibration> cals = lines.stream().map(D07::toCal).toList();
+		final List<Calibration> cals = lines.stream().map(D07::toCalibration).toList();
 		
 		final long result = cals.stream().filter(D07::hasSolution).mapToLong(c -> c.total()).sum();
 		System.out.println(result);
 	}
 	
-	private static Calibration toCal(final String line) {
+	private static Calibration toCalibration(final String line) {
 		final String[] split = line.split(":");
 		final long total = Long.parseLong(split[0]);
 		final List<Long> longs = parseLongsFromString(split[1]);
@@ -51,20 +51,15 @@ public class D07 {
 	}
 
 	private static boolean hasSolution(final Calibration cal) {
-		System.out.println(cal);
-		for (final List<Operator> operatorSet : collectOperators(cal)) {
-//			System.out.println(operatorSet);
-			long result = applyOperators(cal, operatorSet);
-//			System.out.println(result);
-			if (result == cal.total()) {
+		for (final List<Operator> operatorSet : collectOperators(cal.values().size())) {
+			if (applyOperators(cal, operatorSet) == cal.total()) {
 				return true;
 			}
 		}
-//		System.out.println();
 		return false;
 	}
 
-	private static long applyOperators(Calibration cal, List<Operator> operatorSet) {
+	private static long applyOperators(final Calibration cal, final List<Operator> operatorSet) {
 		final List<Long> values = cal.values();
 		long value = values.get(0);
 		for (int i = 0; i < operatorSet.size(); i++) {
@@ -73,20 +68,15 @@ public class D07 {
 		return value;
 	}
 
-	private static List<List<Operator>> collectOperators(Calibration cal) {
-		final int size = cal.values().size();
-		List<List<Operator>> curSet = null;
-		for (int i = 0; i < size - 1; i++) {
-			List<List<Operator>> newSet = new ArrayList<>();
-			if (curSet == null) {
-				newSet = Arrays.stream(Operator.values()).map(o -> Arrays.asList(o)).toList();
-			} else {
-				for (final List<Operator> cur : curSet) {
-					for (final Operator o : Operator.values()) {
-						List<Operator> new0 = new ArrayList<>(cur);
-						new0.add(o);
-						newSet.add(new0);
-					}
+	private static List<List<Operator>> collectOperators(final int size) {
+		List<List<Operator>> curSet = Arrays.stream(Operator.values()).map(o -> Arrays.asList(o)).toList();
+		for (int i = 1; i < size - 1; i++) {
+			final List<List<Operator>> newSet = new ArrayList<>();
+			for (final List<Operator> cur : curSet) {
+				for (final Operator o : Operator.values()) {
+					final List<Operator> new0 = new ArrayList<>(cur);
+					new0.add(o);
+					newSet.add(new0);
 				}
 			}
 			curSet = newSet;

02b8fd2ccee04a087b12ce52aa5198711c1be4fc
Jesse Brand <jessebrand@gmail.com>
2024-12-08 09:48:34 +0100

    Prep D09

---
+++ src/nl/jessebrand/aoc/aoc2024/D09.java
@@ -0,0 +1,13 @@
+package nl.jessebrand.aoc.aoc2024;
+
+import static nl.jessebrand.aoc.Utils.readFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D09 {
+
+	public static void main(String[] args) throws IOException {
+		final List<String> lines = readFile("2024/d09");
+	}
+}

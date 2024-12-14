ccd0e6fdb8d7374d59fa1e2adeb9118109d11be2
Jesse Brand <jessebrand@gmail.com>
2024-12-14 13:16:31 +0100

    Cleanup earlier days.

--- src/nl/jessebrand/aoc/aoc2024/D01.java
+++ src/nl/jessebrand/aoc/aoc2024/D01.java
@@ -1,36 +1,29 @@
 package nl.jessebrand.aoc.aoc2024;
 
-import static nl.jessebrand.aoc.Utils.countOccurances;
+import static nl.jessebrand.aoc.Utils.*;
 import static nl.jessebrand.aoc.Utils.parseColumnsAsInts;
 import static nl.jessebrand.aoc.Utils.readFile;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class D01 {
 
 	public static void main(String[] args) throws IOException {
 		final List<String> lines = readFile("2024/d01");
-		List<List<Integer>> cols = parseColumnsAsInts(lines);
+		final List<List<Integer>> cols = parseColumnsAsInts(lines);
 		for (List<Integer> list : cols) {
 			list.sort(null);
 		}
-		System.out.println(cols);
+		out(cols);
 
 		final int size = cols.get(0).size();
 		final List<Integer> list1 = cols.get(0);
 		final List<Integer> list2 = cols.get(1);
 		
-		int total = 0;
-		for (int i = 0; i < size; i++) {
-			total += Math.abs(list1.get(i) - list2.get(i));
-		}
-		System.out.println(total);
+		out("1: %s", IntStream.range(0, size).map(i -> Math.abs(list1.get(i) - list2.get(i))).sum());
 		
-		int total2 = 0;
-		for (int i : list1) {
-			total2 += i * countOccurances(list2, i);
-		}
-		System.out.println(total2);
+		out("2: %s", list1.stream().mapToLong(i -> i * countOccurances(list2, i)).sum());
 	}
 }

894976407df09d425d08373ceae9a5fb5d366a5a
Jesse Brand <jessebrand@gmail.com>
2025-11-30 11:59:55 +0100

    Prep for AoC 2025

---
+++ src/nl/jessebrand/aoc/aoc2025/D01.java
@@ -0,0 +1,17 @@
+package nl.jessebrand.aoc.aoc2025;
+
+import static nl.jessebrand.aoc.Utils.*;
+import static nl.jessebrand.aoc.Utils.parseColumnsAsInts;
+import static nl.jessebrand.aoc.Utils.readFile;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.IntStream;
+
+public class D01 {
+
+	public static void main(String[] args) throws IOException {
+		final List<String> lines = readFile("2025/d01");
+		out(lines);
+	}
+}

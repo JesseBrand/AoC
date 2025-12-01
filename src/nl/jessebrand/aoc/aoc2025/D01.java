c26d29b668ae09184dbfadcd449983ef8eee26d9
Jesse Brand <jessebrand@gmail.com>
2025-12-01 06:51:41 +0100

    Day 1 solution.

--- src/nl/jessebrand/aoc/aoc2025/D01.java
+++ src/nl/jessebrand/aoc/aoc2025/D01.java
@@ -11,7 +11,44 @@ import java.util.stream.IntStream;
 public class D01 {
 
 	public static void main(String[] args) throws IOException {
-		final List<String> lines = readFile("2025/d01");
+		run("2025/d01ex");
+		run("2025/d01");
+	}
+
+	private static void run(final String file) throws IOException {
+		final List<String> lines = readFile(file);
 		out(lines);
+		
+		int r = 50;
+		int a = 0;
+		int b = 0;
+		
+		for (final String instr : lines) {
+			int v = Integer.parseInt(instr.substring(1));;
+			if (instr.startsWith("L")) {
+				v = -v;
+			}
+			b += Math.abs(v) / 100;
+			v %= 100;
+			if (r + v >= 100) {
+				b++;
+			}
+			if (r > 0 && r + v <= 0) {
+				b++;
+			}
+			r += v + 100;
+			r %= 100;
+			if (r == 0) {
+				a++;
+			}
+//			out("%s > %d - %d/%d", instr, r.value, r.a, r.b);
+		}
+		out("1: %d", a);
+		out("2: %d", b);
+		out();
 	}
 }
+
+// 6580 wrong
+// 5998 wrong
+// 6558

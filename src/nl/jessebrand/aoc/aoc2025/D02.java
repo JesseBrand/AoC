339e1376825241d035be7156acaac974b12a98f3
Jesse Brand <jessebrand@gmail.com>
2025-12-01 06:56:09 +0100

    Prep day 2

---
+++ src/nl/jessebrand/aoc/aoc2025/D02.java
@@ -0,0 +1,15 @@
+package nl.jessebrand.aoc.aoc2025;
+
+import static nl.jessebrand.aoc.Utils.*;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D02 {
+
+	public static void main(String[] args) throws IOException {
+		final List<String> lines = readFile("2025/d02");
+		out(lines);
+	}
+
+}

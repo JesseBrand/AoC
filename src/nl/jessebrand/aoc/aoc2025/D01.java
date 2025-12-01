0ba1a0f911a3c48de0745bccab65bdfc3eaadb4a
Jesse Brand <jessebrand@gmail.com>
2025-12-01 10:42:11 +0100

    Touch-up.

--- src/nl/jessebrand/aoc/aoc2025/D01.java
+++ src/nl/jessebrand/aoc/aoc2025/D01.java
@@ -10,7 +10,7 @@ import java.util.stream.IntStream;
 
 public class D01 {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(final String[] args) throws IOException {
 		run("2025/d01ex");
 		run("2025/d01");
 	}
@@ -19,32 +19,32 @@ public class D01 {
 		final List<String> lines = readFile(file);
 		out(lines);
 		
-		int r = 50;
-		int a = 0;
-		int b = 0;
+		int dial = 50;
+		int r1 = 0;
+		int r2 = 0;
 		
-		for (final String instr : lines) {
-			int v = Integer.parseInt(instr.substring(1));;
-			if (instr.startsWith("L")) {
-				v = -v;
+		for (final String line : lines) {
+			int val = Integer.parseInt(line.substring(1));;
+			if (line.startsWith("L")) {
+				val = -val;
 			}
-			b += Math.abs(v) / 100;
-			v %= 100;
-			if (r + v >= 100) {
-				b++;
+			r2 += Math.abs(val) / 100;
+			val %= 100;
+			if (dial + val >= 100) {
+				r2++;
 			}
-			if (r > 0 && r + v <= 0) {
-				b++;
+			if (dial > 0 && dial + val <= 0) {
+				r2++;
 			}
-			r += v + 100;
-			r %= 100;
-			if (r == 0) {
-				a++;
+			dial += val + 100;
+			dial %= 100;
+			if (dial == 0) {
+				r1++;
 			}
 //			out("%s > %d - %d/%d", instr, r.value, r.a, r.b);
 		}
-		out("1: %d", a);
-		out("2: %d", b);
+		out("1: %d", r1);
+		out("2: %d", r2);
 		out();
 	}
 }

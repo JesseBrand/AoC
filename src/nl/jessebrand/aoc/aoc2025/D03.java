175dc1ad5ff5f355e574649d4339827f50479702
Jesse Brand <jessebrand@gmail.com>
2025-12-03 06:26:25 +0100

    Day 3 solution.

--- src/nl/jessebrand/aoc/aoc2025/D03.java
+++ src/nl/jessebrand/aoc/aoc2025/D03.java
@@ -14,6 +14,31 @@ public class D03 {
 
 	private static void solve(final String file) throws IOException {
 		final List<String> lines = readFile(file);
-		out(lines);
+//		out(lines);
+		long totalA = lines.parallelStream().map(line -> findBiggestX(line, 2)).reduce((a, b) -> a + b).get();
+		long totalB = lines.parallelStream().map(line -> findBiggestX(line, 12)).reduce((a, b) -> a + b).get();
+		out("1: %d", totalA);
+		out("2: %d", totalB);
+	}
+
+	private static long findBiggestX(String line, int count) {
+		int i = highestIndex(line, count - 1);
+		return Long.parseLong("" + line.charAt(i) + (count == 1 ? "" : findBiggestX(line.substring(i + 1), count - 1)));
+	}
+
+	private static final char[] C = {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'};
+	
+	private static int highestIndex(String line, int count) {
+		for (char c : C) {
+			int i = line.indexOf(c);
+			if (i != -1 && i + count < line.length()) {
+//				out("Highest in %s with %d left: %d", line, count, i);
+				return i;
+			}
+		}
+		throw new IllegalStateException("\"" + line + "\" " + count);
 	}
 }
+
+// 17432
+// 173065202451341

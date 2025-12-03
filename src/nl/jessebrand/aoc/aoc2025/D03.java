72025ced4c3747f022f8fd89be39b8beeec1698e
Jesse Brand <jessebrand@gmail.com>
2025-12-03 06:31:02 +0100

    Cleanup

--- src/nl/jessebrand/aoc/aoc2025/D03.java
+++ src/nl/jessebrand/aoc/aoc2025/D03.java
@@ -1,6 +1,7 @@
 package nl.jessebrand.aoc.aoc2025;
 
-import static nl.jessebrand.aoc.Utils.*;
+import static nl.jessebrand.aoc.Utils.out;
+import static nl.jessebrand.aoc.Utils.readFile;
 
 import java.io.IOException;
 import java.util.List;
@@ -15,28 +16,27 @@ public class D03 {
 	private static void solve(final String file) throws IOException {
 		final List<String> lines = readFile(file);
 //		out(lines);
-		long totalA = lines.parallelStream().map(line -> findBiggestX(line, 2)).reduce((a, b) -> a + b).get();
-		long totalB = lines.parallelStream().map(line -> findBiggestX(line, 12)).reduce((a, b) -> a + b).get();
-		out("1: %d", totalA);
-		out("2: %d", totalB);
+		final long totalA = lines.parallelStream().mapToLong(line -> findBiggestX(line, 2)).sum();
+		final long totalB = lines.parallelStream().mapToLong(line -> findBiggestX(line, 12)).sum();
+		out("Part 1: %d", totalA);
+		out("Part 2: %d", totalB);
 	}
 
-	private static long findBiggestX(String line, int count) {
-		int i = highestIndex(line, count - 1);
-		return Long.parseLong("" + line.charAt(i) + (count == 1 ? "" : findBiggestX(line.substring(i + 1), count - 1)));
+	private static long findBiggestX(final String line, final int charCount) {
+		final int i = highestIndex(line, charCount - 1);
+		return Long.parseLong("" + line.charAt(i) + (charCount == 1 ? "" : findBiggestX(line.substring(i + 1), charCount - 1)));
 	}
 
-	private static final char[] C = {'9', '8', '7', '6', '5', '4', '3', '2', '1', '0'};
+	private static final char[] CHARS = "98765431210".toCharArray();
 	
-	private static int highestIndex(String line, int count) {
-		for (char c : C) {
-			int i = line.indexOf(c);
-			if (i != -1 && i + count < line.length()) {
-//				out("Highest in %s with %d left: %d", line, count, i);
+	private static int highestIndex(final String line, final int remaining) {
+		for (char c : CHARS) {
+			final int i = line.indexOf(c);
+			if (i != -1 && i + remaining < line.length()) {
 				return i;
 			}
 		}
-		throw new IllegalStateException("\"" + line + "\" " + count);
+		throw new IllegalArgumentException("\"" + line + "\" " + remaining);
 	}
 }
 

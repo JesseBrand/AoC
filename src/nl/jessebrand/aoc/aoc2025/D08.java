b2a59f061c712658e5be0817cd89d9272269ce9b
Jesse Brand <jessebrand@gmail.com>
2025-12-07 06:44:24 +0100

    Prepare day 8.

---
+++ src/nl/jessebrand/aoc/aoc2025/D08.java
@@ -0,0 +1,23 @@
+package nl.jessebrand.aoc.aoc2025;
+
+import static nl.jessebrand.aoc.Utils.*;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D08 {
+
+	public static void main(final String[] args) throws IOException {
+		solve("2025/d08ex");
+		solve("2025/d08");
+	}
+
+	private static void solve(final String file) throws IOException {
+		final List<String> lines = readFile(file);
+		out(lines);
+
+//		out("Part 1: %d", resultA);
+//		out("Part 2: %d", resultB);
+		out();
+	}
+}

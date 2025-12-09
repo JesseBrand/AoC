22bcd919c348c7a3361c6009e70ba0d3f8f02f0a
Jesse Brand <jessebrand@gmail.com>
2025-12-09 12:53:18 +0100

    Day 10 prep.

---
+++ src/nl/jessebrand/aoc/aoc2025/D10.java
@@ -0,0 +1,24 @@
+package nl.jessebrand.aoc.aoc2025;
+
+import static nl.jessebrand.aoc.Utils.*;
+import static nl.jessebrand.aoc.Utils.readFile;
+
+import java.io.IOException;
+import java.util.List;
+
+public class D10 {
+
+	public static void main(final String[] args) throws IOException {
+		solve("2025/d10ex");
+		solve("2025/d10");
+	}
+
+	private static void solve(final String file) throws IOException {
+		final List<String> lines = readFile(file);
+		out(lines);
+
+//		out("Part 1: %d", totalA);
+//		out("Part 2: %d", totalB);
+		out();
+	}
+}
